0296591d5cbb128067e2bd8a684ac1ad9d37fa34
Rhys <rsimpson@amplience.com>
2022-04-21 17:09:04 +0100

    Add validation for hotspot edit dialog cta.

--- src/app/editor/hotspot-edit-dialog/hotspot-edit-dialog.component.ts
+++ src/app/editor/hotspot-edit-dialog/hotspot-edit-dialog.component.ts
@@ -5,16 +5,25 @@ import { SetHotspotInfoCommand } from 'src/app/services/editor-commands/hotspot-
 @Component({
   selector: 'app-hotspot-edit-dialog',
   templateUrl: './hotspot-edit-dialog.component.html',
-  styleUrls: ['./hotspot-edit-dialog.component.scss']
+  styleUrls: ['./hotspot-edit-dialog.component.scss'],
 })
 export class HotspotEditDialogComponent implements OnInit {
-
-  constructor(private dialogRef: MatDialogRef<HotspotEditDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public hotspot: SetHotspotInfoCommand) { }
-
-  ngOnInit(): void {
-
+  get canSave(): boolean {
+    return (
+      this.hotspot.cta == null ||
+      (this.hotspot.cta.caption != null &&
+        this.hotspot.cta.caption.length > 0 &&
+        this.hotspot.cta.value != null &&
+        this.hotspot.cta.value.length > 0)
+    );
   }
+
+  constructor(
+    private dialogRef: MatDialogRef<HotspotEditDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public hotspot: SetHotspotInfoCommand
+  ) {}
+
+  ngOnInit(): void {}
 
   onCancel(): void {
     this.hotspot.cancelled = true;
@@ -24,12 +33,11 @@ export class HotspotEditDialogComponent implements OnInit {
   ctaToggle(): void {
     if (this.hotspot.cta == null) {
       this.hotspot.cta = {
-        caption: "",
-        value: ""
+        caption: '',
+        value: '',
       };
     } else {
       this.hotspot.cta = undefined;
     }
   }
-
 }
